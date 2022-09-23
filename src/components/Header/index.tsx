7a5fdf73d56123040663f30e887e08d860250da7
samuel ventura <samuelventura.contato@gmail.com>
2022-09-22 23:29:43 -0300

    estilização global e base do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import styles from './header.module.scss';
+
 export default function Header(): JSX.Element {
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/Logo.svg" alt="Space Traveling." />
+        <div className={styles.spaceInHeader}/>
       </div>
-      <div/>
     </header>
   );
 }
